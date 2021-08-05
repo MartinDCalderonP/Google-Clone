31389473173f78fd2232d4f01ddb7a5aecb6c900
Martín Calderón <martincalderon92@outlook.com>
2021-08-05 16:52:23 -0300

    Change styling in HomeJS from CSS to SASS with CSS Modules.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Home.css';
+import styles from './Home.module.scss';
 import { Link } from 'react-router-dom';
 import AppsIcon from '@material-ui/icons/Apps';
 import { Button } from '@material-ui/core';
@@ -7,27 +7,25 @@ import Search from '../components/Search';
 
 export default function Home() {
 	return (
-		<div className="home">
-			<div className="home-header">
+		<div className={styles.home}>
+			<div className={styles.header}>
 				<Link to="/gmail">Gmail</Link>
 				<Link to="/images">Imágenes</Link>
-				<AppsIcon color="disabled" />
-				<Button className="home-header-button" variant="contained">
-					I<span className="home-header-button-span">niciar sesión</span>
+				<AppsIcon />
+				<Button className={styles.loginButton} variant="contained">
+					I<span className={styles.lowercaseSpan}>niciar sesión</span>
 				</Button>
 			</div>
 
-			<div className="home-body">
+			<div className={styles.body}>
 				<img
 					src="https://www.google.com.ar/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
 					alt="Logo de Google"
 				/>
 
-				<div className="home-search-div">
-					<Search />
-				</div>
+				<Search />
 
-				<div className="home__offerBy">
+				<div className={styles.offerBy}>
 					<p>
 						Ofrecido por Google en: <Link to="/english"> English </Link>
 					</p>
